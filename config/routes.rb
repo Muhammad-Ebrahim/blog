5ed5797ef0906b68543163593046e38bfd05877f
Muhammad Ibrahim <muhammed.ibrahim011149@gmail.com>
2018-01-26 15:57:34 +0200

    integrated typeform and mailchimp:anchor:

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   get 'tags/:tag', to: 'posts#index', as: :tag
   resources :posts
+  get 'subscribers/index'
   root to: "pages#home"
 end
