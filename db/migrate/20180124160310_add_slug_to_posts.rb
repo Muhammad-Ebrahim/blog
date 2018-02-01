3326ed43dc6398f47b3d35225fc277c7fc70083e
Muhammad Ibrahim <muhammed.ibrahim011149@gmail.com>
2018-02-01 14:28:58 +0200

    Fixed the problem in the migration

--- db/migrate/20180124160310_add_slug_to_posts.rb
+++ db/migrate/20180124160310_add_slug_to_posts.rb
@@ -1,6 +1,5 @@
 class AddSlugToPosts < ActiveRecord::Migration[5.1]
   def change
-    add_column :posts, :slug, :string
-    add_column :posts, :unique => true
+    add_column :posts, :slug, :string, :unique => true
   end
 end
