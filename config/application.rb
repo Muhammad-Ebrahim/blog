06fcb49c6802fd5f22c2f362b8bc01634be29972
Muhammad Ibrahim <muhammed.ibrahim011149@gmail.com>
2018-02-02 14:29:02 +0200

    fixing ckeditor (6)

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Blog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.autoload_paths += %w(#{config.root}/app/models/ckeditor)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -17,7 +18,7 @@ module Blog
   end
 end
 
-config.autoload_paths += %w(#{config.root}/app/models/ckeditor)
+
 
 #config.assets.precompile += Ckeditor.assets
     #config.assets.precompile += %w( ckeditor_assets/* *.png *.jpg *.jpeg *.gif img/* uploads/*)
